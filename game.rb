386245b031edb0ab3ea851245b146d27d9a86a37
Drew Kang <kangdrew95@gmail.com>
2021-09-13 19:47:58 +0000

    Setting up instance variables

--- game.rb
+++ game.rb
@@ -1,1 +1,19 @@
 require_relative './player'
+
+
+class Game
+
+  attr_reader :winner
+
+  def initialize
+    @winner = nil
+    @current_player = nil
+  end
+
+
+  private
+
+  attr_accessor :current_player
+  attr_writer :winner
+
+end
